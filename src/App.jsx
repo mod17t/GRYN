c75651e70193dd307f4544e18f6d46e704caa8b0
lucasokinda <nathlucas@outlook.fr>
2026-01-15 10:05:40 +0100

    Ajout de la route de la page calculateur

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './pages/Accueil'
 import Footer from './components/UI/Footer'
 import ProfilePage from './Pages/ProfilePage';
+import Calculateur from './pages/Calculateur'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/Profil" element={<ProfilePage />} />
+          <Route path="/Calculateur" element={<Calculateur/>} />
         </Routes>
         <Footer/>
       </main>
